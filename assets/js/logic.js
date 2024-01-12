dddb7f0796ba6d0aedbf0f2b4ee91a53b032a882
Ana Domuta <anadomuta@Anas-MacBook-Pro.local>
2024-01-12 16:53:47 +0000

    set the timer to start upon clicking on the button start quiz

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,18 +1,21 @@
+var startQuizEl = document.getElementById("start");
 var count = 0;
 var timerEl = document.querySelector(".timer");
 var timeEl = document.getElementById("time");
 var secondsLeft = 60;
 var timerInterval;
 
-function setTime() {
-  timerInterval = setInterval(function () {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft + " seconds left.";
+function startQuiz(event) {
+  startQuizEl.addEventListener("click", function setTime() {
+    timerInterval = setInterval(function () {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft + " seconds left.";
 
-    if (secondsLeft === 0) {
-      clearInterval(timerInterval);
-    }
-  }, 1000);
+      if (secondsLeft === 0) {
+        clearInterval(timerInterval);
+      }
+    }, 1000);
+  });
 }
 
-setTime();
+startQuiz();
