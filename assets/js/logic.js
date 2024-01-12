a577efcf461e65e080a8de4fb0dd939493676406
Ana Domuta <anadomuta@Anas-MacBook-Pro.local>
2024-01-12 16:31:11 +0000

    added timer interval function of 60 seconds

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,18 @@
+var count = 0;
+var timerEl = document.querySelector(".timer");
+var timeEl = document.getElementById("time");
+var secondsLeft = 60;
+var timerInterval;
 
+function setTime() {
+  timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left.";
+
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+}
+
+setTime();
