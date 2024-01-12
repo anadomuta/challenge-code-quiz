b590c65b011d9b63f8acb76589e02fbd09cd07f1
Ana Domuta <anadomuta@Anas-MacBook-Pro.local>
2024-01-12 15:58:29 +0000

    added questions to be asked

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,1 +1,47 @@
-console.log("hello");
+var questions = [
+  {
+    title: "Question 1: What is the property textContent?",
+    choices: [
+      "Sets the textual content of a node",
+      "Returns the textual content of a node",
+      "Sets & Returns the textual content of a node",
+      "Modifies texual content",
+    ],
+    answer: "Sets & Returns the textual content of a node",
+    explanation:
+      "Explanation: The property textContent sets or returns the textual content of a node and its descendants. If you set the textContent property, any child nodes are removed and replaced by a single Text node containing the specified string.",
+  },
+  {
+    title: "Question 2: Which of the following is not an error in JavaScript?",
+    choices: [
+      "Missing of Bracket",
+      "Division by zero",
+      "Syntax error",
+      "Missing of semicolons",
+    ],
+    answer: "Division by zero",
+    explanation:
+      "Explanation: In JavaScript, division by zero does not result in an error; it just returns infinity or negative infinity. However, because zero divided by zero has no well-defined value, the result of this operation is the unusual not-a-number value, which is written as NaN.",
+  },
+  {
+    title:
+      "Question 3: Which of the following object is the main entry point to all client-side JavaScript features and APIs?",
+    choices: ["Position", "Window", "Standard", "Location"],
+    answer: "Window",
+    explanation:
+      "Explanation: All client-side JavaScript features and APIs are accessed through the Window object. It represents a web browser window or frame, and the identifier window can be used to refer to it.",
+  },
+  {
+    title:
+      "Question 4: Arrays in JavaScript are defined by which of the following statements?",
+    choices: [
+      "It is an ordered list of values",
+      "It is an ordered list of objects",
+      "It is an ordered list of string",
+      "It is an ordered list of functions",
+    ],
+    answer: "It is an ordered list of values",
+    explanation:
+      "Explanation: An array in JavaScript is an ordered list of values, each value is referred to as an element, and it is identified by an index. An array can include values of many sorts and the length of an array dynamically sized.",
+  },
+];
