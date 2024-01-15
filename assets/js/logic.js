04e1f99a0db1d1f77e32665456c107b1ad04a8cd
Ana Domuta <anadomuta@Anas-MacBook-Pro.local>
2024-01-15 14:32:53 +0000

    created function to display questions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,21 +1,36 @@
 var startQuizEl = document.getElementById("start");
-var count = 0;
 var timerEl = document.querySelector(".timer");
 var timeEl = document.getElementById("time");
+var questionTitleEl = document.getElementById("question-title");
+var questionChoices = document.querySelector(".choices");
+var choicesArray = [];
 var secondsLeft = 60;
+var count = 0;
 var timerInterval;
 
-function startQuiz(event) {
-  startQuizEl.addEventListener("click", function setTime() {
-    timerInterval = setInterval(function () {
-      secondsLeft--;
-      timeEl.textContent = secondsLeft + " seconds left.";
+startQuizEl.addEventListener("click", function setTime() {
+  timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left.";
 
-      if (secondsLeft === 0) {
-        clearInterval(timerInterval);
-      }
-    }, 1000);
-  });
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+});
+
+function renderQuestions() {
+  var questionTitleText = questions.title; //assign question title to h2
+  var choicesArray = questions.choices;
+
+  var listEl = document.createElement("ol");
+  questionTitleEl.textContent = questionTitleText;
 }
 
-startQuiz();
+choicesArray.forEach((choice) => {
+  var listItemEl = document.createElement("li");
+  listItemEl.textContent = choice;
+  listEl.appendChild(listItemEl);
+});
+
+renderQuestions();
